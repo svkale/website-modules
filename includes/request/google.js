dc88c725e5f5a1a61799301cbb4685a5c3659fe1
Siddharth Kale <organizer.sites@gmail.com>
2020-12-28 22:47:16 +0530

    v1

--- includes/request/google.js
+++ includes/request/google.js
@@ -57,18 +57,44 @@ function request_gdoc_published_inline_contents(response_obj)
 //pages
 function request_gsheet_page(response_obj)
 {
-	let gsheet_to_page="";
-	const page_arr=JSON.parse(response_obj.response);
+	let gsheet_to_page="",exec_video_drive_script=0;
+	const page_arr=JSON.parse(response_obj.response)["page_contents"];
 	for(let i=0;i<page_arr.length;i++)
 	{
-		if(page_arr[i][0]=="")
+		if(page_arr[i][0]=="video_drive")
 		{
-			gsheet_to_page+="";
+			gsheet_to_page+="<iframe class=\"media1 request_google_video_drive\" src=\"https://drive.google.com/file/d/"+page_arr[i][1]+"/preview\" style=\"visibility: none;\"></iframe>";
+			exec_video_drive_script=1;
 		}
 		else if(page_arr[i][0]=="")
 		{
-			gsheet_to_page+="";
+			gsheet_to_page+="<div class=\"u1 putins\" data-target-url=\""+page_arr[i][1]+"\" data-function-name=\"request_gdoc_published_inline_contents\"></div>";
 		}
+	}
+	console.log(exec_video_drive_script);
+	if(exec_video_drive_script==1)
+	{
+		setTimeout(function()
+		{
+			const video_drive_script=document.createElement("script");
+			video_drive_script.setAttribute("type","text/javascript");
+			video_drive_script.innerHTML=`
+			let drive_videos=document.getElementsByClassName('request_google_video_drive');
+				for(let i=0;i<drive_videos.length;i++)
+				{
+					drive_videos[i].onload=function()
+					{
+						drive_videos[i].style.height=(drive_videos[i].offsetWidth*9/16)+"px";
+						drive_videos[i].style.visibility="visible";
+					};
+					window.addEventListener("resize",function()
+					{
+						drive_videos[i].style.height=(drive_videos[i].offsetWidth*9/16)+"px";
+					});
+				}
+			`;
+			document.getElementsByTagName('html')[0].appendChild(video_drive_script);
+		},500);
 	}
 	return gsheet_to_page;
 }
