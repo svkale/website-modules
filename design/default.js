2a42d41c4a214fe9e9ca4386c6194277926025f5
Siddharth Kale <organizer.sites@gmail.com>
2021-02-07 22:20:01 +0530

    v1

--- design/default.js
+++ design/default.js
@@ -2,5 +2,9 @@ const html_main=document.getElementsByTagName('main')[0],html_header=document.ge
 html_main.style.minHeight=(window.innerHeight-html_header.scrollHeight-html_footer.scrollHeight)+"px";
 window.addEventListener("resize",function()
 {
-	html_main.style.minHeight=(window.innerHeight-html_header.scrollHeight-html_footer.scrollHeight)+"px";
+	main_page_format();
 });
+function main_page_format()
+{
+	html_main.style.minHeight=(window.innerHeight-html_header.scrollHeight-html_footer.scrollHeight)+"px";
+}
