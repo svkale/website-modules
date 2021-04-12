7c968540931bd54acd19fb1eb982c65d8eb17cc1
sites-admin <organizer.sites@gmail.com>
2021-04-12 15:40:28 +0530

    v1

--- elements/media/img/custom-header.js
+++ elements/media/img/custom-header.js
@@ -1,14 +1,15 @@
 let xmlhttp=new XMLHttpRequest();
 xmlhttp.onreadystatechange=function()
 {
+	console.log(this);
 	if(this.status==404)
 	{
-		document.querySelector("header img:nth-child(1)").src='./images/logo.jpg';
+		document.querySelector("header img:nth-child(1)").src='/images/logo.jpg';
 	}
 	else
 	{
 		document.querySelector("header img:nth-child(1)").src='./images/'+location.pathname.split("/")[1]+'.jpg';
 	}
 };
-xmlhttp.open('HEAD','./images/'+location.pathname.split("/")[1]+'.jpg',true); 
+xmlhttp.open('HEAD','/images/'+location.pathname.split("/")[1]+'.jpg',true); 
 xmlhttp.send();
