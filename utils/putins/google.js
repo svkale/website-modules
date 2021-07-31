be2736cc9cf3bd97a376622f1e9219491c759737
Siddharth Kale <69069085+svkale@users.noreply.github.com>
2021-07-31 10:25:49 +0530

    Update google.js

--- utils/putins/google.js
+++ utils/putins/google.js
@@ -2,7 +2,7 @@
 function gsrfn(query,target_id)
 {
 	var p="https://script.google.com/macros/s/AKfycbwotnrn5vDVzCWhSsWIPE1apYuPyxEIjd39lBtxeT0XqGM0_pPqJuKBICHcB1_aiQ_cbg/exec?"+query;
-	request_promise(p).then((res)=>if(document.getElementById(target_id)) {document.getElementById(target_id).innerHTML=res.responseText;});
+	request_promise(p).then((res)=>{if(document.getElementById(target_id)) {document.getElementById(target_id).innerHTML=res.responseText;}});
 }
 
 var doc_ele_HTML,style_loaded=0;
