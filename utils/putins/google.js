7768146826103fef404216282c0d1911f54ba7f2
Siddharth Kale <69069085+svkale@users.noreply.github.com>
2021-07-31 17:59:23 +0530

    Update google.js

--- utils/putins/google.js
+++ utils/putins/google.js
@@ -496,13 +496,13 @@ function putins_make_subpage_from_HTML(dom,doc_ele,element)
 	{
 		let doc_html=dom.documentElement.innerHTML;
 		let html_text='<span>★ Rating: </span><span id=SRV_mytc></span><span>&nbsp;&nbsp;&nbsp;&nbsp;<select name="StarRating" id="StarRating" required onchange=alert("Thank_you_for_your_response.");this.disabled=true;gsrfn("fn=StarRating&value="+this.value+"&page="+(document.URL).replace("#","///")); ><option disabled selected value> -- Rate This Page -- </option><option value="1">&#9733;</option><option value="2">&#9733;&#9733;</option> <option value="3">&#9733;&#9733;&#9733;</option><option value="4">&#9733;&#9733;&#9733;&#9733;</option> <option value="5">&#9733;&#9733;&#9733;&#9733;&#9733;</option></select></span>';
-		let notice_target=dom.evaluate("//*[contains(.,'"+html_text+"')]",dom,null, XPathResult.ANY_TYPE,null);
-		let eval_text='gsrfn("fn=StarRating&page="+(document.URL).replace("#","///"),"SRV_mytc");';
-		let fs=document.createElement("script");
-		fs.setAttribute("type","text/javascript");
-		fs.innerText=eval_text;
-		document.getElementsByTagName('html')[0].appendChild(fs);
-		dom.documentElement.innerHTML=dom.documentElement.innerHTML.replace(doc_html.substring(doc_html.search("{StarRating}"),doc_html.search("{/StarRating}")+12),"");
+		
+		let srscript=document.createElement("script");
+		srscript.setAttribute("type","text/javascript");
+		srscript.innerHTML='gsrfn("fn=StarRating&page="+(document.URL).replace("#","///"),"SRV_mytc");';
+		document.getElementsByTagName('html')[0].appendChild(srscript);
+		
+		dom.documentElement.innerHTML=dom.documentElement.innerHTML.replace(doc_html.substring(doc_html.search("{StarRating}"),doc_html.search("{/StarRating}")+13),html_text);
 	}
 
 	doc_ele.innerHTML="";
