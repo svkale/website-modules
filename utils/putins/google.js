da8949f7200e8a2c99f6a4a66d1c7fb0dbc7f599
sites-admin <kalesiddharthv@gmail.com>
2021-10-20 13:27:26 +0530

    v1

--- utils/putins/google.js
+++ utils/putins/google.js
@@ -489,7 +489,10 @@ function putins_make_subpage_from_HTML(dom,doc_ele,element)
 		let doc_html=dom.documentElement.innerHTML;
 
 		[...dom.documentElement.querySelectorAll("p")]
-			.filter(p => p.innerText == doc_text.substring(doc_text.search("{html}"),doc_text.search("{/html}")+7))[0].innerHTML = "<div class=\"cont1\">"+doc_text.substring(doc_text.search("{html}")+6,doc_text.search("{/html}"))+"</div>";
+			.filter(p => {
+				console.log(doc_text.substring(doc_text.search("{html}"),doc_text.search("{/html}")+7));
+				return p.innerText == doc_text.substring(doc_text.search("{html}"),doc_text.search("{/html}")+7);
+			})[0].innerHTML = "<div class=\"cont1\">"+doc_text.substring(doc_text.search("{html}")+6,doc_text.search("{/html}"))+"</div>";
 
 		// dom.documentElement.innerHTML=dom.documentElement.innerHTML.replace(doc_html.substring(doc_html.search("{html}"),doc_html.search("{/html}")+7),"<div class=\"cont1\">"+doc_text.substring(doc_text.search("{html}")+6,doc_text.search("{/html}"))+"</div>");
 	}
